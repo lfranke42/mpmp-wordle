f42e3f4bf00eaecc32fc84d441a88490e1e7548a
Luca <luca_niklas.franke@stud.htwk-leipzig.de>
2023-09-24 17:34:55 +0200

    add loop for getting user input, fix cpu bugs

--- wordle.asm
+++ wordle.asm
@@ -1,4 +1,7 @@
-  ldc %reg0 0x00
-  ldc %reg1 0x77
-  st %reg0 %reg1
-  ld %reg4 %reg0
+main_loop:
+  ldc %reg0 0x8002 ; address for reading the keyboard buffer
+  ld %reg1 %reg0 ; load the current input buffer value to reg1
+  ldc %reg0 0x8000 ; address for writing to the screen
+  st %reg0 %reg1 ; write the current input buffer value to the screen
+  nop
+  jr main_loop
