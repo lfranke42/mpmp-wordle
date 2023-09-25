edc2f8bef206549c1c2d62e9c694576c576db082
Luca <luca_niklas.franke@stud.htwk-leipzig.de>
2023-09-25 15:59:50 +0200

    add title screen, load word to guess into memory

--- wordle.asm
+++ wordle.asm
@@ -1,9 +1,92 @@
 init_game:
-  ldc
+  ldc %reg0 0x0100 ; base adress of the word to guess
 
+  ;; store the word to guess to adress 0x0100 - 0x0104
+  ldc %reg1 0x72
+  st %reg0 %reg1
+  inc %reg0
+  ldc %reg1 0x6F
+  st %reg0 %reg1
+  inc %reg0
+  ldc %reg1 0x63
+  st %reg0 %reg1
+  inc %reg0
+  ldc %reg1 0x6B
+  st %reg0 %reg1
+  inc %reg0
+  ldc %reg1 0x79
+  st %reg0 %reg1
+
+
+display_title:
+  ldc %reg0 0x8000 ; address for writing to the screen
+
+  ;; WORDLE
+  ldc %reg1 0x57
+  st %reg0 %reg1
+  ldc %reg1 0x4F
+  st %reg0 %reg1
+  ldc %reg1 0x52
+  st %reg0 %reg1
+  ldc %reg1 0x44
+  st %reg0 %reg1
+  ldc %reg1 0x4C
+  st %reg0 %reg1
+  ldc %reg1 0x45
+  st %reg0 %reg1
+
+  ;; -
+  ldc %reg1 0x20
+  st %reg0 %reg1
+  ldc %reg1 0x2D
+  st %reg0 %reg1
+  ldc %reg1 0x20
+  st %reg0 %reg1
+
+  ;; Enter
+  ldc %reg1 0x45
+  st %reg0 %reg1
+  ldc %reg1 0x6E
+  st %reg0 %reg1
+  ldc %reg1 0x74
+  st %reg0 %reg1
+  ldc %reg1 0x65
+  st %reg0 %reg1
+  ldc %reg1 0x72
+  st %reg0 %reg1
+  ldc %reg1 0x20
+  st %reg0 %reg1
+
+  ;; your
+  ldc %reg1 0x79
+  st %reg0 %reg1
+  ldc %reg1 0x6F
+  st %reg0 %reg1
+  ldc %reg1 0x75
+  st %reg0 %reg1
+  ldc %reg1 0x72
+  st %reg0 %reg1
+  ldc %reg1 0x20
+  st %reg0 %reg1
+
+  ;; guess:
+  ldc %reg1 0x67
+  st %reg0 %reg1
+  ldc %reg1 0x75
+  st %reg0 %reg1
+  ldc %reg1 0x65
+  st %reg0 %reg1
+  ldc %reg1 0x73
+  st %reg0 %reg1
+  st %reg0 %reg1
+  ldc %reg1 0x3A
+  st %reg0 %reg1
+
+  ;; new line
+  ldc %reg1 0x0A
+  st %reg0 %reg1
 
 ;; handle user input and store results to memory (address 0x004 - 0x008)
-
 get_user_input:
   ldc %reg0 0x00
   ld %reg1 %reg0 ; load the amount of chars parsed
